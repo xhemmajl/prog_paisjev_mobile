58670a5fb3cab5cafb1f82603a6578a32bbe4af9
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-06-01 23:26:25 +0200

    Me ja nis me ba vizatimin e peshkut ?
    pe provoj me bitmap , canvas ?

--- app/src/main/java/com/example/fishjump/GameView.java
+++ app/src/main/java/com/example/fishjump/GameView.java
@@ -1,4 +1,22 @@
 package com.example.fishjump;
 
-public class GameView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class GameView extends View {
+
+    public GameView(Context context) {
+        super(context);
+    }
+
+
+
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
 }
+
