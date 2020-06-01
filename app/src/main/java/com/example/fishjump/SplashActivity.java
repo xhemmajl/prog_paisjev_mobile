79c30dd9093387dc0cec4609890c7c3b273b3769
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2020-06-01 22:52:07 +0200

    po punoj pamjen e par tlojes,
    Dmth e bana ni dizajn qaty emri lojes , nni background , ni foto edhe butonin play edhe ja bana intentin qe kur klikohet me hin pamje tlojes kur ta bojm e tvendosmi qka qysh tek .

--- app/src/main/java/com/example/fishjump/SplashActivity.java
+++ app/src/main/java/com/example/fishjump/SplashActivity.java
@@ -2,13 +2,31 @@ package com.example.fishjump;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
-
+    Button Play;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Pe lidhum qat buttonin Play qe e kemi te activity qe kur ta klikojm permes intentav me dal te display i lojes (qe se kemi kriju hala niher, pe boj niher mainActivity se nashta qaty e implementoj gameViewin)
+
+        Play = (Button) findViewById(R.id.playButton);
+
+        Play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent playGame = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(playGame);
+            }
+        });
+
+
     }
+
 }
