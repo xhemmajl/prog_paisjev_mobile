93f2224b2d90337387f0708d9f4597c7a3a345db
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-06-01 23:39:32 +0200

    N'mainactivity e lidha obj e gameView per testim

--- app/src/main/java/com/example/fishjump/MainActivity.java
+++ app/src/main/java/com/example/fishjump/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+       //pe deklaroj qetu prej qasaj gameview qe e banum , per me ba launch tash mainactivity
+    private GameView pamjaLojes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //qetu e bona ni obj prej klases gameview po ja la niher emrin pamjaLojes.
+        pamjaLojes = new GameView(this);
+        setContentView(pamjaLojes);
     }
 }
