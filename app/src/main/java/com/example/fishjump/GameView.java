5f1831033c12774c4ac610de4aed730b4e44bbb2
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-06-01 23:32:26 +0200

    e provova qat fish1 , najs po del

--- app/src/main/java/com/example/fishjump/GameView.java
+++ app/src/main/java/com/example/fishjump/GameView.java
@@ -5,6 +5,10 @@ import android.graphics.Canvas;
 import android.view.View;
 
 public class GameView extends View {
+        private Bitmap fish;
+//qet foto peshkit i gjeta ngoogle i morra , ish edhe versioni 2 najs kur klikon psh me i hap kraht
+        //dmth po e vizatojm peshkin e par per momentin
+        fish = BitmapFactory.decodeResource(getResources(),R.drawable.fish1);
 
     public GameView(Context context) {
         super(context);
