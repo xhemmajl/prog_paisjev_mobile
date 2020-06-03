7d878041ada4d00735a322f91dd93d61613d9c14
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-06-03 18:39:35 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/stickmanjump/MainActivity.java
+++ app/src/main/java/com/example/stickmanjump/MainActivity.java
@@ -5,10 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private FlyingFishView gameView;
+    private Handler handler = new Handler();
+    private final static long Interval = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        gameView = new FlyingFishView(this);
+        setContentView(gameView);
+
+        Timer timer =  new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        gameView.invalidate();
+                    }
+                });
+            }
+        },0,Interval);
     }
 }
