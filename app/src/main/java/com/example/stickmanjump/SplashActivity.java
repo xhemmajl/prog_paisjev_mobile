974cb768ffac1543a75a9245f24c6731cde89c8f
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-06-03 18:37:57 +0200

    Update SplashActivity.java

--- app/src/main/java/com/example/stickmanjump/SplashActivity.java
+++ app/src/main/java/com/example/stickmanjump/SplashActivity.java
@@ -1,14 +1,28 @@
 package com.example.stickmanjump;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
+ Button Play;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Play = (Button) findViewById(R.id.playButton);
+
+        Play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent play = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(play);
+            }
+        });
     }
 }
