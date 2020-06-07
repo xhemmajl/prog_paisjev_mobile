58c57e7d93457d08636863d01888bba4acc872a7
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-06-08 00:01:46 +0200

    Update GameView.java

--- app/src/main/java/com/example/stickmanjump/GameView.java
+++ app/src/main/java/com/example/stickmanjump/GameView.java
@@ -223,5 +223,8 @@ public class GameView extends View {
         canvas.drawBitmap(korona,koronaX,koronaY,null);
         canvas.drawText("Level :"+level,20,170,levelPaint);
         canvas.drawText("Score :",20,60,scorePaint);
+        if(level>7){
+            canvas.drawBitmap(korona2,korona2X,korona2Y,null);
+        }
     }
     }
