4da5d40690137ed347bf04d956f0aca7cd17fe12
Valton <valtoni.gl@live.com>
2020-06-10 02:21:34 +0200

    Duke e funksionalizu klasen e GameOver
    Krijova nje  button te cilin e lidha me ID e buttonit ne layout, po ashtu edhe 2 labelat i inicalizova , per score edhe highscore.

--- app/src/main/java/com/example/stickmanjump/GameOverActivity.java
+++ app/src/main/java/com/example/stickmanjump/GameOverActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GameOverActivity extends AppCompatActivity {
+        private Button StartGameAgain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+        TextView scoreLabel =  (TextView) findViewById(R.id.scoreLabel);
+        TextView highScoreLabel = (TextView) findViewById(R.id.highScoreLabel);
+         StartGameAgain = (Button) findViewById(R.id.play_again_btn);
+
     }
 }
