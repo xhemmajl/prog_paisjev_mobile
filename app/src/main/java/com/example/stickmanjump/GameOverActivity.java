8a3c0a3aa00259b9122728d9b1ed1c1b39d801a0
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2020-06-10 02:29:22 +0200

    Update GameOverActivity.java

--- app/src/main/java/com/example/stickmanjump/GameOverActivity.java
+++ app/src/main/java/com/example/stickmanjump/GameOverActivity.java
@@ -3,8 +3,10 @@ package com.example.stickmanjump;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
