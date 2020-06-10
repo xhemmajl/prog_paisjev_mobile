47727241a4184ace82a9e7235753014b1bb28f3a
Valton <valtoni.gl@live.com>
2020-06-10 02:23:17 +0200

    Importimi Buttonit edhe TextView

--- app/src/main/java/com/example/stickmanjump/GameOverActivity.java
+++ app/src/main/java/com/example/stickmanjump/GameOverActivity.java
@@ -3,6 +3,8 @@ package com.example.stickmanjump;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameOverActivity extends AppCompatActivity {
         private Button StartGameAgain;
