fc2ea8e16bf3f505ae8659b788d8589f34274d7f
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2020-06-17 02:50:31 +0200

    Importova klasat
    I importova se nuk u ekzekutojke!

--- app/src/main/java/com/example/stickmanjump/Registration.java
+++ app/src/main/java/com/example/stickmanjump/Registration.java
@@ -1,8 +1,13 @@
 package com.example.stickmanjump;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Registration extends AppCompatActivity {
     EditText Password;
