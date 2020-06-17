6f642929411e29c7de414e272b5915a0aaa70d30
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2020-06-17 02:34:26 +0200

    Funksionalizimi i LoginActivity!
    Po e funksionalizoj aktivitetin e Loginit.

--- app/src/main/java/com/example/stickmanjump/LoginActivity.java
+++ app/src/main/java/com/example/stickmanjump/LoginActivity.java
@@ -1,10 +1,20 @@
 package com.example.stickmanjump;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText Email;
+    EditText Password;
+    Button Login;
+    TextView Registration;
+    DatabaseHelper db;
+    ConstraintLayout constrainLayout2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
