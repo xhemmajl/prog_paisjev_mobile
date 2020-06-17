95ff33b1749e78b578f05d92f77e20e07e0dc555
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2020-06-17 02:35:36 +0200

    Krijimi i eventListenerit per butonin Login

--- app/src/main/java/com/example/stickmanjump/LoginActivity.java
+++ app/src/main/java/com/example/stickmanjump/LoginActivity.java
@@ -3,7 +3,9 @@ package com.example.stickmanjump;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -25,5 +27,16 @@ public class LoginActivity extends AppCompatActivity {
         Login = (Button) findViewById(R.id.loginbtn);
         db = new DatabaseHelper(this);
         constrainLayout2 = findViewById(R.id.constrainLayout2);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = Email.getText().toString();
+                String password = Password.getText().toString();
+
+                Boolean Chkemailpass = db.emailpassword(email, password);
+                if (Chkemailpass == true) {
+                    Intent logInt = new Intent(LoginActivity.this, SplashActivity.class);
+                    startActivity(logInt);
+                }
     }
 }
