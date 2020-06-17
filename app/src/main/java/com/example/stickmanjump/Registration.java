3437a1575ec7d1c27adc38889b862cb286ee10b7
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-06-17 02:43:55 +0200

    Funksionalizmi i klases Registration

--- app/src/main/java/com/example/stickmanjump/Registration.java
+++ app/src/main/java/com/example/stickmanjump/Registration.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Registration extends AppCompatActivity {
+    EditText Password;
+    EditText confirm_password;
+    EditText Email;
+    Button SignUp;
+    TextView SignIn;
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
