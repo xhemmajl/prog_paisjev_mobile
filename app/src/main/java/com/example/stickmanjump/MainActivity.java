2a8ea4dd95620491d1a79e1d0bed0e29c39e2ae8
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-06-20 03:42:04 +0200

    Paraqitja e kontekstit nmainactivity
    Ni koment : 
    
    Ne si ekip, para se ma ba komita ktu , kemi 2-3 workspace qe punojm deri sa trregullohet qysh deshirojm ne , dhe pasi te rregulllohen errorat , pastaj i bajm commit ne github . (Pra ne nuk bejm kommit gjithqka qka punojm) - Prandaj mos mendoni se jemi  tpa gabueshem :D

--- app/src/main/java/com/example/stickmanjump/MainActivity.java
+++ app/src/main/java/com/example/stickmanjump/MainActivity.java
@@ -30,3 +30,13 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        pager = findViewById(R.id.viewpager);
+        mTabLayout = findViewById(R.id.tablayout);
+
+        firstItem = findViewById(R.id.firstItem);
+        secondItem = findViewById(R.id.secondItem);
+        thirdItem = findViewById(R.id.thirditem);
+
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
