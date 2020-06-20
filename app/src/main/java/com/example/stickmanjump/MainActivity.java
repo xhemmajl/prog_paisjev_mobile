ae87b4dfbe3db738ede119c35dd3debabb78bb19
Valton <valtoni.gl@live.com>
2020-06-20 03:39:10 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/stickmanjump/MainActivity.java
+++ app/src/main/java/com/example/stickmanjump/MainActivity.java
@@ -1,37 +1,18 @@
-package com.example.stickmanjump;
+package com.example.flyingfishgame;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.content.Intent;
-import android.os.Handler;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class MainActivity extends AppCompatActivity {
-    private GameView pamjaLojes;
-    private Handler handler = new Handler();
-    private final static long Interval = 30;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        pamjaLojes = new GameView(this);
-        setContentView(pamjaLojes);
-
-        Timer timer =  new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        pamjaLojes.invalidate();
-                    }
-                });
-            }
-        },0,Interval);
-    }
-}
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
