bafa90375187655f7f81998e07123746a19b143c
Valton <valtoni.gl@live.com>
2020-06-20 03:44:05 +0200

    Update PagerAdapter.java

--- app/src/main/java/com/example/stickmanjump/PagerAdapter.java
+++ app/src/main/java/com/example/stickmanjump/PagerAdapter.java
@@ -1,4 +1,9 @@
-package com.example.stickmanjump;
+package com.example.flyingfishgame;
 
-public class PagerAdapter {
-}
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    private int tabsNumber;
