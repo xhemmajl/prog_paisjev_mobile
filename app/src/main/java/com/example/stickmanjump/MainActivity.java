ea90b75e5e29d9c3dd4a6741bf81d1de53312d6e
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-06-20 03:42:36 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/stickmanjump/MainActivity.java
+++ app/src/main/java/com/example/stickmanjump/MainActivity.java
@@ -48,3 +48,25 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
 
         adapter = new PagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,mTabLayout.getTabCount());
         pager.setAdapter(adapter);
+        
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
+
+    }
