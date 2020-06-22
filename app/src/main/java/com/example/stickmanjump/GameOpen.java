cd9e591827b1bc5e7b01690f0e888e7df1ceb1a6
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-06-23 00:40:28 +0200

    Update GameOpen.java

--- app/src/main/java/com/example/stickmanjump/GameOpen.java
+++ app/src/main/java/com/example/stickmanjump/GameOpen.java
@@ -2,7 +2,12 @@ package com.example.stickmanjump;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GameOpen extends AppCompatActivity {
 
