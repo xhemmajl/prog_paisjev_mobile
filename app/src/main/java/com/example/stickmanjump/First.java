4274889a8d33a9fa25b605b094c3912cea012c42
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2020-06-23 00:24:58 +0200

    Update First.java

--- app/src/main/java/com/example/stickmanjump/First.java
+++ app/src/main/java/com/example/stickmanjump/First.java
@@ -32,7 +32,13 @@ public class First extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_login, container, false);
+        View view =  inflater.inflate(R.layout.activity_login, container, false);
+        Email = (EditText) view.findViewById(R.id.email);
+        Password = (EditText) view.findViewById(R.id.password);
+        Login = (Button) view.findViewById(R.id.loginbtn);
+        db = new DatabaseHelper(getContext());
+        constrainLayout2 = view.findViewById(R.id.constrainLayout2);
+
+        return view;
     }
 }
