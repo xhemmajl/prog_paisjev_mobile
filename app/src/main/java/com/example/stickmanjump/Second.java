ebcfab1c466602318e15e8ebb1d22861f835f5fb
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2020-06-23 00:31:09 +0200

    Update Second.java

--- app/src/main/java/com/example/stickmanjump/Second.java
+++ app/src/main/java/com/example/stickmanjump/Second.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Second extends Fragment {
     EditText Password;
