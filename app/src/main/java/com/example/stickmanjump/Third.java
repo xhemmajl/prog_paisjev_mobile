efa7921e0a019605692e69e1fb5c1c3b372fc414
Valton <valtoni.gl@live.com>
2020-06-24 04:09:10 +0200

    Update Third.java

--- app/src/main/java/com/example/stickmanjump/Third.java
+++ app/src/main/java/com/example/stickmanjump/Third.java
@@ -19,5 +19,37 @@ public class Third extends Fragment {
         openurl.setData(Uri.parse(url));
         startActivity(openurl);
     }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view3 =  inflater.inflate(R.layout.fragment_third, container, false);
+
+        view3.findViewById(R.id.open_fb).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked("https://facebook.com/ourpage");
+            }
+        });
+
+        view3.findViewById(R.id.open_insta).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked("https://instagram.com/ourpage");
+            }
+        });
+
+        view3.findViewById(R.id.open_yt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked("https://youtube.com/ourpage");
+            }
+        });
+
+
+        return view3;
+    }
+
+}
 
 
