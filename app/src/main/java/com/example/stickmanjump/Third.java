7b5ff39a9b3191a3ab4ddaae74a97a5ee4b464d5
Valton <valtoni.gl@live.com>
2020-06-24 04:08:59 +0200

    Update Third.java

--- app/src/main/java/com/example/stickmanjump/Third.java
+++ app/src/main/java/com/example/stickmanjump/Third.java
@@ -3,9 +3,21 @@ package com.example.stickmanjump;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+public class Third extends Fragment {
+
+    public Third() {
+        // Required empty public constructor
+    }
+    public void clicked(String url){
+        Intent openurl = new Intent(Intent.ACTION_VIEW);
+        openurl.setData(Uri.parse(url));
+        startActivity(openurl);
+    }
 
 
