5b60eecdc978733447b87b2b2781782f8e0aee27
Valton <valtoni.gl@live.com>
2020-06-24 03:55:57 +0200

    Update HowToPlay.java

--- app/src/main/java/com/example/stickmanjump/HowToPlay.java
+++ app/src/main/java/com/example/stickmanjump/HowToPlay.java
@@ -5,10 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HowToPlay extends AppCompatActivity {
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_play);
+        
+         back = (Button) findViewById(R.id.backButton);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(howtoplay.this,MainActivity.class);
+                startActivity(back);
+            }
+        });
     }
 }
