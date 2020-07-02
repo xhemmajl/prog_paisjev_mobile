2d6e6f272f6686a42588da08b93fc8429985a5e9
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2020-07-02 05:19:53 +0200

    Konfigurimi Call Klases

--- app/src/main/java/com/example/stickmanjump/call.java
+++ app/src/main/java/com/example/stickmanjump/call.java
@@ -1,8 +1,13 @@
 package com.example.stickmanjump;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class call extends AppCompatActivity {
 
@@ -11,7 +16,7 @@ public class call extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_call_us);
+        setContentView(R.layout.activity_call);
 
 
         call = findViewById(R.id.btncall);
@@ -24,11 +29,7 @@ public class call extends AppCompatActivity {
         callIntent.setData(Uri.parse("tel:049136072"));
         //qekjo nevojitet me kqyr se a e ka te lejum permisionin per me ba thirrje enabled perdoruesi!
         callIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        if (ActivityCompat.checkSelfPermission(call_us.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(call_us.this,new String[]{Manifest.permission.CALL_PHONE},CALL_PERMISION_CODE);
 
-            return;
-        }
 
         startActivity(callIntent);
 
