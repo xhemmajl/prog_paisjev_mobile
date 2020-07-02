19198d4e17eea03aeb7ca39472bfb379b9be811b
Valton <valtoni.gl@live.com>
2020-07-02 05:17:10 +0200

    Update call.java

--- app/src/main/java/com/example/stickmanjump/call.java
+++ app/src/main/java/com/example/stickmanjump/call.java
@@ -6,9 +6,31 @@ import android.os.Bundle;
 
 public class call extends AppCompatActivity {
 
+    ImageButton call;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_call);
+        setContentView(R.layout.activity_call_us);
+
+
+        call = findViewById(R.id.btncall);
+
+
     }
-}
+
+    public void callEvent(View v) {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:049136072"));
+        //qekjo nevojitet me kqyr se a e ka te lejum permisionin per me ba thirrje enabled perdoruesi!
+        callIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        if (ActivityCompat.checkSelfPermission(call_us.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(call_us.this,new String[]{Manifest.permission.CALL_PHONE},CALL_PERMISION_CODE);
+
+            return;
+        }
+
+        startActivity(callIntent);
+
+
+    }}
