864e88b2188142710e09f04b7455954b253ed07e
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2020-07-02 05:22:39 +0200

    Update call.java

--- app/src/main/java/com/example/stickmanjump/call.java
+++ app/src/main/java/com/example/stickmanjump/call.java
@@ -3,7 +3,9 @@ package com.example.stickmanjump;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -31,6 +33,16 @@ public class call extends AppCompatActivity {
         callIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         startActivity(callIntent);
 
 
