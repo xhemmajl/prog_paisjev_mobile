7a501a88e414d606fa22f9ebba42854f00fb10cb
xhemmajl <44348785+xhemmajl@users.noreply.github.com>
2020-07-02 16:46:12 +0200

    Inicimi i FireBase
    Jemi duke e perdorur FireBase ,  qe useri mundet me ba upload HighScoren e vet, dhe te ruhet ne firebase

---
+++ app/src/main/java/com/example/stickmanjump/UploadHighScore.java
@@ -0,0 +1,14 @@
+package com.example.stickmanjump;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadHighScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_high_score);
+    }
+}
